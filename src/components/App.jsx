a864cf3a098a0794f3f07839f0877a5c2d0cd008
Olevova <olevova1983@gmail.com>
2022-12-27 20:12:36 +0200

    make change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,51 @@
 import React from "react";
+import { useState} from "react";
 import { Section } from "./Section/Section";
 import { Statistic } from "./Statistics/Statistics";
 import { FeedbackOptions } from "./Feedback/Feedback";
 import { Notification } from "./Notification/Notification";
 
 
-export class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  // state = {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+  // }
+
+  const[good, setStateGood] = useState('');
+  const[neutral, setStateNeutral] = useState('');
+  const[bad, setStatebad] = useState('');
+  // const[countTotalFeedback, setTotalFedback] =setState('');
+
+  const handleIncreament = (key) => {
+    switch (key) {
+      case "good":
+        setStateGood(state => Number(state)+1);
+        break;
+      case "neutral":
+        setStateNeutral(state => Number(state)+1);
+          break;
+      case "bad":
+        setStatebad(state => Number(state)+1);
+          break;
+      default:
+        break;
+    }
+
   }
+  
 
-  handleIncreament = (key) => {
-    this.setState(value => (
-      {
-        [key]: value[key] + 1
-      }
-    ));
-
-  }
-
-  countTotalFeedback = () => {
-    const arrayTotal = Object.values(this.state);
-    return arrayTotal.reduce((value, count) => value + count
-    )
+const countTotalFeedback = () => Number(good + neutral + bad);
        
-  }
 
-  countPositiveFeedbackPercentage = () =>
-    Math.round((this.state.good / this.countTotalFeedback()) * 100)
-    
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = Object.keys(this.state);
+
+const countPositiveFeedbackPercentage = () => Math.round((good / (good+neutral+bad)) * 100)
+
+  // countPositiveFeedbackPercentage = () =>
+  //   Math.round((this.state.good / this.countTotalFeedback()) * 100)
+    const options = ['good',"neutral","bad"];
+  
     return (
       <div
         style={{
@@ -47,23 +59,21 @@ export class App extends React.Component {
         <Section title={"Please Leave Feedback"}>
         <FeedbackOptions
           options={options}
-          onLeaveFeedback={this.handleIncreament}
+          onLeaveFeedback={handleIncreament}
         />
         </Section>
-        {this.countTotalFeedback() <= 0 ? <Notification message="Not Feedback given"/>:
+        {countTotalFeedback() <= 0 ? <Notification message="Not Feedback given"/>:
         <>
         <Section title="Statistic">
         <Statistic 
         good={good}
         neutral={neutral}
         bad={bad}
-        total={this.countTotalFeedback()}
-        positivePercentage = {this.countPositiveFeedbackPercentage()}
+        total={countTotalFeedback()}
+        positivePercentage = {countPositiveFeedbackPercentage()}
         />
         </Section>
         </>
         }
       </div>
       )};
-      
-}
