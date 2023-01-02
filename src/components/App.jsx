5e01171afeb1af4469f59fe6c70b33dec47329c2
Olevova <olevova1983@gmail.com>
2023-01-02 10:44:39 +0200

    end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,16 +6,9 @@ import { FeedbackOptions } from './Feedback/Feedback';
 import { Notification } from './Notification/Notification';
 
 export const App = () => {
-  // state = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  // }
-
   const [good, setStateGood] = useState('');
   const [neutral, setStateNeutral] = useState('');
   const [bad, setStatebad] = useState('');
-  // const[countTotalFeedback, setTotalFedback] =setState('');
 
   const handleIncreament = key => {
     switch (key) {
@@ -38,8 +31,6 @@ export const App = () => {
   const countPositiveFeedbackPercentage = () =>
     Math.round((good / (good + neutral + bad)) * 100);
 
-  // countPositiveFeedbackPercentage = () =>
-  //   Math.round((this.state.good / this.countTotalFeedback()) * 100)
   const options = ['good', 'neutral', 'bad'];
 
   return (
